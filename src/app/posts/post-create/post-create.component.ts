a34cc1c63b24c13373c151f5a46bdf3d5930cd48
Nadira K <nkassymkhanova@cube-erp.com>
2022-11-02 23:55:02 +0600

    section 2

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateCompenent {
+  enteredTitle = '';
+  enteredContent = '';
+
+  constructor(public postsService: PostService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
+  }
+}
